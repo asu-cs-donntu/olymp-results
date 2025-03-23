2ac38919a52b52ee1f7b64a9a150421e683af8c1
i888844 <i888844@yandex.ru>
2025-03-23 22:52:31 +0300

    fix table for mobile devices

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -16,29 +16,33 @@ function App() {
                                 Народной
                                 Республики по информатике и компьютерным технологиям</h3>
                         </div>
-                        <table className="table table-striped">
-                            <thead>
-                            <tr>
-                                <th scope="col">Занятое место</th>
-                                <th scope="col">Фамилия, имя</th>
-                                <th scope="col">Учебное заведение</th>
-                                <th scope="col">Набрано баллов</th>
-                                <th scope="col">Допуск*</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {ResultsData.map((result, index) => (
-                                <tr key={index}>
-                                    <td>{result.place}</td>
-                                    <td>{result.name}</td>
-                                    <td>{result.school}</td>
-                                    <td>{result.score}</td>
-                                    <td>{result.allowance}</td>
+                        <div className="table-container">
+                            <table className="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Занятое место</th>
+                                    <th scope="col">Фамилия, имя</th>
+                                    <th scope="col">Учебное заведение</th>
+                                    <th scope="col">Набрано баллов</th>
+                                    <th scope="col">Допуск*</th>
                                 </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                        <p>* — На заключительный тур проходят участники, показавшие наилучшие результаты в первом этапе (не более 40 человек). Отметка "+" - участник допущен к заключительному туру, отметка "—" - не допущен.</p>
+                                </thead>
+                                <tbody>
+                                {ResultsData.map((result, index) => (
+                                    <tr key={index}>
+                                        <td>{result.place}</td>
+                                        <td>{result.name}</td>
+                                        <td>{result.school}</td>
+                                        <td>{result.score}</td>
+                                        <td>{result.allowance}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
+                        <p>* — На заключительный тур проходят участники, показавшие наилучшие результаты в первом этапе
+                            (не более 40 человек). Отметка "+" - участник допущен к заключительному туру, отметка "—" -
+                            не допущен.</p>
                     </section>
                 </div>
             </div>
