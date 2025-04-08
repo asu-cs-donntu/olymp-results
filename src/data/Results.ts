c1ebfdda0261496ab922a94bde76069c9ad10bae
i888844 <i888844@yandex.ru>
2025-04-08 11:00:01 +0300

    fix table data and heading

--- src/data/Results.ts
+++ src/data/Results.ts
@@ -2,307 +2,307 @@ export const ResultsData = [
     {
         place: 1,
         id: '2026203290',
-        scores: {total: '100', level1: '100', level2: '', level3: ''},
+        scores: { total: '100', level1: '40', level2: '30', level3: '30' },
         time: '01:18:55'
     },
     {
         place: 2,
         id: '2026198975',
-        scores: {total: '98', level1: '98', level2: '', level3: ''},
+        scores: { total: '98', level1: '39', level2: '29', level3: '30' },
         time: '01:10:52'
     },
     {
         place: 3,
         id: '2026204372',
-        scores: {total: '98', level1: '98', level2: '', level3: ''},
+        scores: { total: '98', level1: '39', level2: '29', level3: '30' },
         time: '01:20:54'
     },
     {
         place: 4,
         id: '2026260811',
-        scores: {total: '98', level1: '98', level2: '', level3: ''},
+        scores: { total: '98', level1: '39', level2: '29', level3: '30' },
         time: '02:51:11'
     },
     {
         place: 5,
         id: '2026200568',
-        scores: {total: '91', level1: '91', level2: '', level3: ''},
+        scores: { total: '91', level1: '31', level2: '30', level3: '30' },
         time: '01:13:56'
     },
     {
         place: 6,
         id: '2026229322',
-        scores: {total: '91', level1: '91', level2: '', level3: ''},
+        scores: { total: '91', level1: '31', level2: '30', level3: '30' },
         time: '02:01:48'
     },
     {
         place: 7,
         id: '2026180046',
-        scores: {total: '89', level1: '89', level2: '', level3: ''},
+        scores: { total: '89', level1: '30', level2: '29', level3: '30' },
         time: '00:36:40'
     },
     {
         place: 8,
         id: '2026197083',
-        scores: {total: '88', level1: '88', level2: '', level3: ''},
+        scores: { total: '88', level1: '29', level2: '30', level3: '29' },
         time: '01:07:26'
     },
     {
         place: 9,
         id: '2026231555',
-        scores: {total: '85', level1: '85', level2: '', level3: ''},
+        scores: { total: '85', level1: '28', level2: '28', level3: '29' },
         time: '02:05:01'
     },
     {
         place: 10,
         id: '2026200549',
-        scores: {total: '84', level1: '84', level2: '', level3: ''},
+        scores: { total: '84', level1: '28', level2: '28', level3: '28' },
         time: '01:13:55'
     },
     {
         place: 11,
         id: '2026182072',
-        scores: {total: '83', level1: '83', level2: '', level3: ''},
+        scores: { total: '83', level1: '28', level2: '28', level3: '27' },
         time: '00:40:47'
     },
     {
         place: 11,
         id: '2026206369',
-        scores: {total: '83', level1: '83', level2: '', level3: ''},
+        scores: { total: '83', level1: '28', level2: '28', level3: '27' },
         time: '01:24:37'
     },
     {
         place: 11,
         id: '2026240092',
-        scores: {total: '83', level1: '83', level2: '', level3: ''},
+        scores: { total: '83', level1: '28', level2: '28', level3: '27' },
         time: '02:16:39'
     },
     {
         place: 11,
         id: '2026260013',
-        scores: {total: '83', level1: '83', level2: '', level3: ''},
+        scores: { total: '83', level1: '28', level2: '28', level3: '27' },
         time: '02:49:50'
     },
     {
         place: 15,
         id: '2026220253',
-        scores: {total: '79', level1: '79', level2: '', level3: ''},
+        scores: { total: '79', level1: '26', level2: '26', level3: '27' },
         time: '01:48:56'
     },
     {
         place: 16,
         id: '2026219950',
-        scores: {total: '78', level1: '78', level2: '', level3: ''},
+        scores: { total: '78', level1: '26', level2: '26', level3: '26' },
         time: '01:48:27'
     },
     {
         place: 16,
         id: '2026249921',
-        scores: {total: '78', level1: '78', level2: '', level3: ''},
+        scores: { total: '78', level1: '26', level2: '26', level3: '26' },
         time: '02:33:25'
     },
     {
         place: 18,
         id: '2026181390',
-        scores: {total: '77', level1: '77', level2: '', level3: ''},
+        scores: { total: '77', level1: '26', level2: '25', level3: '26' },
         time: '00:39:27'
     },
     {
         place: 19,
         id: '2026219970',
-        scores: {total: '76', level1: '76', level2: '', level3: ''},
+        scores: { total: '76', level1: '25', level2: '25', level3: '26' },
         time: '01:48:28'
     },
     {
         place: 20,
         id: '2026212551',
-        scores: {total: '74', level1: '74', level2: '', level3: ''},
+        scores: { total: '74', level1: '24', level2: '25', level3: '25' },
         time: '01:35:40'
     },
     {
         place: 21,
         id: '2026175030',
-        scores: {total: '73', level1: '73', level2: '', level3: ''},
+        scores: { total: '73', level1: '24', level2: '24', level3: '25' },
         time: '00:26:39'
     },
     {
         place: 22,
         id: '2026190770',
-        scores: {total: '71', level1: '71', level2: '', level3: ''},
+        scores: { total: '71', level1: '24', level2: '23', level3: '24' },
         time: '00:56:35'
     },
     {
         place: 22,
         id: '2026206918',
-        scores: {total: '71', level1: '71', level2: '', level3: ''},
+        scores: { total: '71', level1: '24', level2: '23', level3: '24' },
         time: '01:25:41'
     },
     {
         place: 22,
         id: '2026266223',
-        scores: {total: '71', level1: '71', level2: '', level3: ''},
+        scores: { total: '71', level1: '24', level2: '23', level3: '24' },
         time: '03:00:11'
     },
     {
         place: 25,
         id: '2026198845',
-        scores: {total: '69', level1: '69', level2: '', level3: ''},
+        scores: { total: '69', level1: '23', level2: '23', level3: '23' },
         time: '01:10:39'
     },
     {
         place: 26,
         id: '2026170627',
-        scores: {total: '64', level1: '64', level2: '', level3: ''},
+        scores: { total: '64', level1: '22', level2: '21', level3: '21' },
         time: '00:18:18'
     },
     {
         place: 26,
         id: '2026201764',
-        scores: {total: '64', level1: '64', level2: '', level3: ''},
+        scores: { total: '64', level1: '22', level2: '21', level3: '21' },
         time: '01:16:05'
     },
     {
         place: 26,
         id: '2026242554',
-        scores: {total: '64', level1: '64', level2: '', level3: ''},
+        scores: { total: '64', level1: '22', level2: '21', level3: '21' },
         time: '02:20:50'
     },
     {
         place: 29,
         id: '2026197175',
-        scores: {total: '61', level1: '61', level2: '', level3: ''},
+        scores: { total: '61', level1: '20', level2: '20', level3: '21' },
         time: '01:07:35'
     },
     {
         place: 29,
         id: '2026198245',
-        scores: {total: '61', level1: '61', level2: '', level3: ''},
+        scores: { total: '61', level1: '20', level2: '20', level3: '21' },
         time: '01:09:32'
     },
     {
         place: 31,
         id: '2026182362',
-        scores: {total: '60', level1: '60', level2: '', level3: ''},
+        scores: { total: '60', level1: '20', level2: '20', level3: '20' },
         time: '00:41:16'
     },
     {
         place: 31,
         id: '2026184534',
-        scores: {total: '60', level1: '60', level2: '', level3: ''},
+        scores: { total: '60', level1: '20', level2: '20', level3: '20' },
         time: '00:45:05'
     },
     {
         place: 31,
         id: '2026198159',
-        scores: {total: '60', level1: '60', level2: '', level3: ''},
+        scores: { total: '60', level1: '20', level2: '20', level3: '20' },
         time: '01:09:21'
     },
     {
         place: 31,
         id: '2026244450',
-        scores: {total: '60', level1: '60', level2: '', level3: ''},
+        scores: { total: '60', level1: '20', level2: '20', level3: '20' },
         time: '02:24:06'
     },
     {
         place: 35,
         id: '2026227914',
-        scores: {total: '57', level1: '57', level2: '', level3: ''},
+        scores: { total: '57', level1: '19', level2: '19', level3: '19' },
         time: '01:59:54'
     },
     {
         place: 36,
         id: '2026274601',
-        scores: {total: '56', level1: '56', level2: '', level3: ''},
+        scores: { total: '56', level1: '19', level2: '18', level3: '19' },
         time: '03:13:24'
     },
     {
         place: 37,
         id: '2026180757',
-        scores: {total: '55', level1: '55', level2: '', level3: ''},
+        scores: { total: '55', level1: '18', level2: '18', level3: '19' },
         time: '00:38:07'
     },
     {
         place: 37,
         id: '2026181222',
-        scores: {total: '55', level1: '55', level2: '', level3: ''},
+        scores: { total: '55', level1: '18', level2: '18', level3: '19' },
         time: '00:39:04'
     },
     {
         place: 39,
         id: '2026244787',
-        scores: {total: '53', level1: '53', level2: '', level3: ''},
+        scores: { total: '53', level1: '18', level2: '17', level3: '18' },
         time: '02:24:41'
     },
     {
         place: 40,
         id: '2026180533',
-        scores: {total: '51', level1: '51', level2: '', level3: ''},
+        scores: { total: '51', level1: '17', level2: '17', level3: '17' },
         time: '00:37:40'
     },
     {
         place: 41,
         id: '2026169159',
-        scores: {total: '49', level1: '49', level2: '', level3: ''},
+        scores: { total: '49', level1: '16', level2: '17', level3: '16' },
         time: '00:15:26'
     },
     {
         place: 41,
         id: '2026230034',
-        scores: {total: '49', level1: '49', level2: '', level3: ''},
+        scores: { total: '49', level1: '16', level2: '17', level3: '16' },
         time: '02:02:47'
     },
     {
         place: 43,
         id: '2026249411',
-        scores: {total: '46', level1: '46', level2: '', level3: ''},
+        scores: { total: '46', level1: '15', level2: '16', level3: '15' },
         time: '02:32:33'
     },
     {
         place: 43,
         id: '2026251194',
-        scores: {total: '46', level1: '46', level2: '', level3: ''},
+        scores: { total: '46', level1: '15', level2: '16', level3: '15' },
         time: '02:35:30'
     },
     {
         place: 45,
         id: '2026228245',
-        scores: {total: '44', level1: '44', level2: '', level3: ''},
+        scores: { total: '44', level1: '15', level2: '14', level3: '15' },
         time: '02:00:22'
     },
     {
         place: 46,
         id: '2026172993',
-        scores: {total: '42', level1: '42', level2: '', level3: ''},
+        scores: { total: '42', level1: '14', level2: '14', level3: '14' },
         time: '00:22:42'
     },
     {
         place: 47,
         id: '2026179827',
-        scores: {total: '38', level1: '38', level2: '', level3: ''},
+        scores: { total: '38', level1: '13', level2: '13', level3: '12' },
         time: '00:36:14'
     },
     {
         place: 48,
         id: '2026199363',
-        scores: {total: '35', level1: '35', level2: '', level3: ''},
+        scores: { total: '35', level1: '12', level2: '12', level3: '11' },
         time: '01:11:38'
     },
     {
         place: 49,
         id: '2026263254',
-        scores: {total: '29', level1: '29', level2: '', level3: ''},
+        scores: { total: '29', level1: '10', level2: '10', level3: '9' },
         time: '02:55:14'
     },
     {
         place: 50,
         id: '2026267767',
-        scores: {total: '27', level1: '27', level2: '', level3: ''},
+        scores: { total: '27', level1: '9', level2: '9', level3: '9' },
         time: '03:02:35'
     },
     {
         place: 51,
         id: '2026190053',
-        scores: {total: '20', level1: '20', level2: '', level3: ''},
+        scores: { total: '20', level1: '7', level2: '7', level3: '6' },
         time: '00:55:15'
     }
 ];
